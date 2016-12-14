ef42bfc13a56da42f61fb894cfe5f17351420870
Thomas Koenig <thomas.koenig@sueddeutsche.de>
2016-12-14 14:36:13 +0100

    trigger gutti

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function gutti(req, res, next) {
 
 var server = restify.createServer({name:'guttiMachine'});
 server.get('/gutti', gutti);
-server.get('/', restify.serveStatic({directory: './public', default: 'index.html'}));
+server.get('/:file', restify.serveStatic({directory: './public', default: 'index.html'}));
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
